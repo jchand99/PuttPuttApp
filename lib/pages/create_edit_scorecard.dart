a77b6316129a1098d49bdb57240ded7465d45b9f
jchand99 <chandlerje@vcu.edu>
2021-02-26 16:35:42 -0500

    Added page files

---
+++ lib/pages/create_edit_scorecard.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class CreateEditScorecardPage extends StatefulWidget {
+  @override
+  _CreateEditScorecardPageState createState() =>
+      _CreateEditScorecardPageState();
+}
+
+class _CreateEditScorecardPageState extends State<CreateEditScorecardPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/pages/hole.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HolePage extends StatefulWidget {
+  @override
+  _HolePageState createState() => _HolePageState();
+}
+
+class _HolePageState extends State<HolePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
