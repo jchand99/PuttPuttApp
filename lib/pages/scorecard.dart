ed2462cf5187d0570cace2d89589dfc835fc9db8
betancourtaj <betancourtaj@vcu.edu>
2021-03-02 16:54:41 -0500

    Added homeScreen functionality

--- lib/pages/scorecard.dart
+++ lib/pages/scorecard.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 
 class ScorecardPage extends StatefulWidget {
+  final String scoreCard;
+  const ScorecardPage(this.scoreCard);
+
   @override
   _ScorecardPageState createState() => _ScorecardPageState();
 }
@@ -8,6 +11,25 @@ class ScorecardPage extends StatefulWidget {
 class _ScorecardPageState extends State<ScorecardPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.scoreCard),
+      ),
+      body: _body(context),
+    );
+  }
+
+  Widget _body(BuildContext context) {
+    return ListView.separated(
+      itemCount: 4,
+      itemBuilder: (context, index) {
+        return ListTile(
+          title: Text('Hole $index'),
+        );
+      },
+      separatorBuilder: (context, index) => const Divider(
+        color: Colors.white,
+      ),
+    );
   }
 }
