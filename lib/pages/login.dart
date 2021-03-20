98b67351332382fe1e7683e5baa8b065354be864
betancourtaj <betancourtaj@vcu.edu>
2021-03-20 19:53:30 -0400

    Added login with icon

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -8,6 +8,48 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Center(
+        child: _buildColumn(context)
+      ),
+    );
+  }
+
+  Widget _buildColumn(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Container(
+          height: 275,
+          width: 250,
+          padding: EdgeInsets.only(top: 100),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(200),
+          ),
+          child: Center(
+            child: Icon(Icons.sports_golf_rounded, size: 200),
+          ),
+        ),
+        Container(
+            padding: const EdgeInsets.fromLTRB(12, 40, 12, 12),
+            child: TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: '@ Email',
+                filled: true,
+                fillColor: Colors.white,
+              ),
+            )),
+        Container(
+            padding: const EdgeInsets.all(12),
+            child: TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: ' Password',
+                filled: true,
+                fillColor: Colors.white,
+              ),
+            ))
+      ],
+    );
   }
 }
