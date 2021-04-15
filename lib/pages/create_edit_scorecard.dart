313521aa70680da93c9d9d5349e6afb83c89f495
jchand99 <chandlerje@vcu.edu>
2021-04-15 19:07:00 -0400

    Made the settings page display current settings

--- lib/pages/create_edit_scorecard.dart
+++ lib/pages/create_edit_scorecard.dart
@@ -61,6 +61,25 @@ class _CreateEditScorecardPageState extends State<CreateEditScorecardPage> {
   }
 
   Widget _buildEditMode(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          TextButton(
+            child: Text("Save", style: TextStyle(color: Colors.white)),
+            onPressed: () => onSave(),
+          )
+        ],
+        title: Center(
+          child: Text('${widget._title} Scorecard'),
+        ),
+      ),
+      body: _buildEditScrollPage(context),
+    );
+  }
+
+  // TODO: Make it so they can actually make changes!
+  // and generally just clean up this class somehow
+  Widget _buildEditScrollPage(BuildContext context) {
     // Create streambuilder for specific scorecard
     return StreamBuilder<DocumentSnapshot>(
       stream: FirebaseFirestore.instance
@@ -79,155 +98,201 @@ class _CreateEditScorecardPageState extends State<CreateEditScorecardPage> {
               child: Text(
                   'There must be an issue with your internet connection!'));
 
-        return Column(
-          children: [
-            Column(
-              children: <Widget>[
-                Container(
-                  padding: const EdgeInsets.all(12.0),
-                  child: TextField(
-                      onChanged: (value) {
-                        setState(() {
-                          _scorecardName = value;
-                        });
-                      },
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: snapshot.data['name'],
-                        filled: true,
-                        fillColor: Colors.grey[200],
-                      )),
-                ),
-                Container(
-                  padding: const EdgeInsets.only(
-                    left: 12,
-                    top: 0,
-                    right: 12,
-                    bottom: 0,
+        _players = snapshot.data['players'];
+
+        return Column(children: <Widget>[
+          Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 5,
+                    blurRadius: 7,
+                    offset: Offset(0, 3), // changes position of shadow
                   ),
-                  child: SwitchListTile(
-                    title: const Text('Par', style: TextStyle(fontSize: 16)),
-                    value: snapshot.data['par_enabled'],
-                    onChanged: (bool value) {
+                ],
+              ),
+              child: _buildUpperButtonLayoutEDIT(context, snapshot.data)),
+          Expanded(child: _buildPlayerListEDIT(context, snapshot.data)),
+        ]);
+      },
+    );
+  }
+
+  Widget _buildPlayerListEDIT(BuildContext context, data) {
+    return Container(
+      child: ListView.separated(
+        padding: const EdgeInsets.all(16),
+        itemCount: data['players'].length,
+        itemBuilder: (context, index) {
+          return Card(
+              elevation: 10,
+              child: ListTile(
+                title: Container(
+                  padding: const EdgeInsets.only(bottom: 10),
+                  child: TextField(
+                    onChanged: (value) {
                       setState(() {
-                        par = value;
+                        data['players'][index]['name'] = value;
                       });
                     },
-                    secondary: !snapshot.data['par_enabled']
-                        ? const Icon(Icons.flag_outlined)
-                        : const Icon(Icons.flag_rounded),
+                    style: TextStyle(
+                      fontSize: 18,
+                    ),
+                    decoration: InputDecoration(
+                        filled: false,
+                        labelText: data['players'][index]['name']),
                   ),
                 ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(
-                        left: 26,
-                        top: 0,
-                        right: 26,
-                        bottom: 0,
-                      ),
-                      child: Align(
-                        child: Text(
-                          'Players',
-                          style: TextStyle(fontSize: 16),
-                        ),
-                        alignment: Alignment.centerLeft,
-                      ),
-                    ),
-                    Expanded(
-                        child: Padding(
-                      padding: const EdgeInsets.only(
-                        left: 26,
-                        top: 12,
-                        right: 26,
-                        bottom: 16,
-                      ),
-                      child: Align(
-                          child: ElevatedButton(
-                              onPressed: () => _createPlayer(context),
-                              child: Text("+", style: TextStyle(fontSize: 16))),
-                          alignment: Alignment.centerRight),
-                    ))
-                  ],
-                )
-              ],
-            ), // TOP LEVEL BUTTONS
-            Container(
-              child: ListView.separated(
-                padding: const EdgeInsets.all(16),
-                itemCount: snapshot.data['players'].length,
-                itemBuilder: (context, index) {
-                  var players = snapshot.data['players'];
+                leading: _ballColorEDIT(context, index, data),
+                trailing: data['players'].length != 1
+                    ? IconButton(
+                        icon: Icon(Icons.highlight_remove_outlined),
+                        onPressed: () {
+                          _removeCard(index);
 
-                  return Card(
-                      elevation: 10,
-                      child: ListTile(
-                        title: //Text(_names[index]['player']),
-                            Padding(
-                          padding: const EdgeInsets.only(bottom: 10),
-                          child: Container(
-                            //height: 4,
-                            child: TextField(
-                              onChanged: (value) {
-                                setState(() {
-                                  players[index]['name'] = value;
-                                });
-                              },
-                              style: TextStyle(
-                                fontSize: 18,
-                              ),
-                              decoration: InputDecoration(
-                                  filled: false, labelText: 'Player $index'),
-                            ),
-                          ),
-                        ),
-                        leading: Stack(
-                            alignment: AlignmentDirectional.center,
-                            children: <Widget>[
-                              Icon(
-                                Icons.circle,
-                                size: 32,
-                                color: ColorPicker.getColorFromString(
-                                    players[index]['color']),
-                              ),
-                              IconButton(
-                                icon: Icon(Icons.edit,
-                                    color: Colors.black, size: 22),
-                                onPressed: () => Nav.pushFullScreenDialog(
-                                    context,
-                                    ColorSelectPage(
-                                      players[index]['name'],
-                                      widget._id,
-                                      onChanged: (value) {
-                                        setState(() {
-                                          _players[index]['color'] = value;
-                                        });
-                                      },
-                                    )),
-                              ),
-                            ]),
-                        trailing: players.length != 1
-                            ? IconButton(
-                                icon: Icon(Icons.highlight_remove_outlined),
-                                onPressed: () {
-                                  _removeCard(index);
-                                })
-                            : SizedBox(
-                                width: 0,
-                                height: 0,
-                              ),
-                      ));
-                },
-                separatorBuilder: (context, index) => Divider(
-                  color: Color.fromRGBO(0, 0, 0, 0),
-                ),
+                          FirebaseFirestore.instance
+                              .collection('users')
+                              .doc(FirebaseAuth.instance.currentUser.uid)
+                              .collection('scorecards')
+                              .doc(widget._id)
+                              .update({'players': _players});
+                        })
+                    : SizedBox(
+                        width: 0,
+                        height: 0,
+                      ),
+              ));
+        },
+        separatorBuilder: (context, index) => Divider(
+          color: Color.fromRGBO(0, 0, 0, 0),
+        ),
+      ),
+    );
+  }
+
+  Widget _ballColorEDIT(BuildContext context, int index, data) {
+    return Stack(alignment: AlignmentDirectional.center, children: <Widget>[
+      Icon(
+        Icons.circle,
+        size: 32,
+        color: ColorPicker.getColorFromString(data['players'][index]['color']),
+      ),
+      IconButton(
+        icon: Icon(Icons.edit, color: Colors.black, size: 22),
+        onPressed: () => Nav.pushFullScreenDialog(
+            context,
+            ColorSelectPage(
+              data['players'][index]['name'],
+              widget._id,
+              onChanged: (value) {
+                setState(() {
+                  _players[index]['color'] = value;
+                  data['players'][index]['color'] = value;
+
+                  FirebaseFirestore.instance
+                      .collection('users')
+                      .doc(FirebaseAuth.instance.currentUser.uid)
+                      .collection('scorecards')
+                      .doc(widget._id)
+                      .update({'players': _players});
+                });
+              },
+            )),
+      ),
+    ]);
+  }
+
+  Widget _buildUpperButtonLayoutEDIT(BuildContext context, data) {
+    return Column(
+      children: <Widget>[
+        Container(
+          padding: const EdgeInsets.all(12.0),
+          child: TextField(
+              onChanged: (value) {
+                setState(() {
+                  _scorecardName = value;
+                });
+              },
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: data['name'],
+                filled: true,
+                fillColor: Colors.grey[200],
+              )),
+        ),
+        Container(
+          padding: const EdgeInsets.only(
+            left: 12,
+            top: 0,
+            right: 12,
+            bottom: 0,
+          ),
+          child: SwitchListTile(
+            title: const Text('Par', style: TextStyle(fontSize: 16)),
+            value: data['par_enabled'],
+            onChanged: (bool value) {
+              setState(() {
+                // TODO: Possible delete?
+                par = value;
+                FirebaseFirestore.instance
+                    .collection('users')
+                    .doc(FirebaseAuth.instance.currentUser.uid)
+                    .collection('scorecards')
+                    .doc(widget._id)
+                    .update({'par_enabled': value});
+              });
+            },
+            secondary: !data['par_enabled']
+                ? const Icon(Icons.flag_outlined)
+                : const Icon(Icons.flag_rounded),
+          ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(
+                left: 26,
+                top: 0,
+                right: 26,
+                bottom: 0,
               ),
-            )
+              child: Align(
+                child: Text(
+                  'Players',
+                  style: TextStyle(fontSize: 16),
+                ),
+                alignment: Alignment.centerLeft,
+              ),
+            ),
+            Expanded(
+                child: Padding(
+              padding: const EdgeInsets.only(
+                left: 26,
+                top: 12,
+                right: 26,
+                bottom: 16,
+              ),
+              child: Align(
+                  child: ElevatedButton(
+                      onPressed: () {
+                        _createPlayer(context);
+
+                        FirebaseFirestore.instance
+                            .collection('users')
+                            .doc(FirebaseAuth.instance.currentUser.uid)
+                            .collection('scorecards')
+                            .doc(widget._id)
+                            .update({'players': _players});
+                      },
+                      child: Text("+", style: TextStyle(fontSize: 16))),
+                  alignment: Alignment.centerRight),
+            ))
           ],
-        );
-      },
+        )
+      ],
     );
   }
 
@@ -316,7 +381,9 @@ class _CreateEditScorecardPageState extends State<CreateEditScorecardPage> {
               ),
               child: Align(
                   child: ElevatedButton(
-                      onPressed: () => _createPlayer(context),
+                      onPressed: () {
+                        _createPlayer(context);
+                      },
                       child: Text("+", style: TextStyle(fontSize: 16))),
                   alignment: Alignment.centerRight),
             ))
@@ -359,8 +426,8 @@ class _CreateEditScorecardPageState extends State<CreateEditScorecardPage> {
                 style: TextStyle(
                   fontSize: 18,
                 ),
-                decoration:
-                    InputDecoration(filled: false, labelText: 'Player $index'),
+                decoration: InputDecoration(
+                    filled: false, labelText: 'Player ${index + 1}'),
               ),
             ),
           ),
@@ -403,39 +470,44 @@ class _CreateEditScorecardPageState extends State<CreateEditScorecardPage> {
   }
 
   onSave() async {
-    // Set the data on save button clicked
-    // The data is sent to the database with the current scorecard id as
-    // a json object
-    FirebaseFirestore.instance
-        .collection('users')
-        .doc(widget._firebaseUser.uid)
-        .collection('scorecards')
-        .doc(widget._id)
-        .set({
-      'players': _players, // TODO: Array of player objects
-      'par_enabled': par,
-      'hole_count': 0,
-      'name': _scorecardName,
-    });
+    if (!widget.editMode) {
+      // Set the data on save button clicked
+      // The data is sent to the database with the current scorecard id as
+      // a json object
+      FirebaseFirestore.instance
+          .collection('users')
+          .doc(widget._firebaseUser.uid)
+          .collection('scorecards')
+          .doc(widget._id)
+          .set({
+        'players': _players, // TODO: Array of player objects
+        'par_enabled': par,
+        'hole_count': 0,
+        'name': _scorecardName,
+      });
 
-    // Grab the document that contains the scorecard count for this user
-    var scorecardCountDoc = await FirebaseFirestore.instance
-        .collection('users')
-        .doc(widget._firebaseUser.uid)
-        .get();
+      // Grab the document that contains the scorecard count for this user
+      var scorecardCountDoc = await FirebaseFirestore.instance
+          .collection('users')
+          .doc(widget._firebaseUser.uid)
+          .get();
 
-    // Get the scorecard count
-    var count = scorecardCountDoc['scorecard_count'];
+      // Get the scorecard count
+      var count = scorecardCountDoc['scorecard_count'];
 
-    // Increment the score by 1 (because they just made a new one)
-    // and then update the value in the database
-    FirebaseFirestore.instance
-        .collection('users')
-        .doc(widget._firebaseUser.uid)
-        .update({'scorecard_count': count + 1});
+      // Increment the score by 1 (because they just made a new one)
+      // and then update the value in the database
+      FirebaseFirestore.instance
+          .collection('users')
+          .doc(widget._firebaseUser.uid)
+          .update({'scorecard_count': count + 1});
 
-    // Pop the page
-    Nav.pop(context);
+      // Pop the page
+      Nav.pop(context);
+    } else {
+      // EDIT MODE DETAILS
+      // TODO: Push all chanegs to the database
+    }
   }
 
   void _removeCard(int index) {
